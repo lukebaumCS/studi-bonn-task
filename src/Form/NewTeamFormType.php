d15201c5601b6b25d79fd33770f523ad72ab5945
Torge Rau <s37trau@uni-bonn.de>
2025-07-27 23:57:22 +0200

    Created first sketch of new Team form

---
+++ src/Form/NewTeamFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Team;
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class NewTeamFormType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options){
+        $builder
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a Name'
+                    ]),
+                ],
+            ])
+            ->add("users", EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'email',
+                'multiple' => true,
+                'attr' => ['class' => 'select2'] 
+            ]);
+    }
+}
